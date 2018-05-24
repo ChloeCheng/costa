6b35cbc0437cc91b861de04cda1f72e62a5ace0a
tristan.dong <tristan.dong@vipshop.com>
2018-05-24 21:46:31 +0800

    积分页面

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 App({
-  
+  global:{
+    currentLanguage:'zh',
+    en:{
+      points:{
+        flag: '积分',
+        title:'积分中心',
+        vipName:'红卡会员',
+        label1:'当前积分',
+        label2: '距离升级积分',
+        label3: '本月底将过期',
+        tab1:'积分兑换',
+        tab2: '最新活动',
+        placeholder:'输入您的转赠积分',
+        btnTxt:'分享给好友',
+        subTitle:'积分记录',
+      },
+    },
+    zh:{
+
+    }
+  },
   onLaunch: function () {
 
   }
