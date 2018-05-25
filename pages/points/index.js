56d8680bc656d07c9a6e7be0964b25021f82260c
tristan.dong <tristan.dong@vipshop.com>
2018-05-25 22:53:38 +0800

    积分logo

--- pages/points/index.js
+++ pages/points/index.js
@@ -6,7 +6,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    currentData: app.global[app.global['currentLanguage']]
+    currentData: app.global[app.global['currentLanguage']],
+    images:{
+      logo:'../../assets/point/vip-logo.png',
+      bg: '../../assets/point/bg.png',
+    },
+    cacheData:{
+      points:{
+        current:1,
+        upgrade:550,
+        expired:50,
+        next:800,
+      }
+    },
   },
 
   /**
