66bdc4b57aeaa40d382bc7b39b8b87ed76ddcb00
tristan.dong <tristan.dong@vipshop.com>
2018-05-27 09:24:41 +0800

    youzan ui

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,3 +1,5 @@
+const Dialog = require('../../components/vendor/dist/dialog/dialog');
+
 Page({
   data: {
     multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物']],
@@ -34,6 +36,15 @@ Page({
         }
       ]],
     multiIndex: [0, 0],
+  },
+  handleClick() {
+    Dialog({
+      title: 'title',
+      message: 'message',
+      selector: '#zan-dialog-test'
+    }).then((res) => {
+      console.log(res);
+    })
   },
   bindMultiPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
