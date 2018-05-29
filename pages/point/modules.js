148112a76c6f99058eef8fa1fbbf8c9e60e4eece
tristan.dong <tristan.dong@vipshop.com>
2018-05-29 20:39:56 +0800

    积分记录

--- pages/point/modules.js
+++ pages/point/modules.js
@@ -21,7 +21,6 @@ exports.getPoint = function (callback) {
     }
   )
 }
-
 exports.getRecord = function (callback) {
   ajax.request(
     'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
@@ -30,15 +29,15 @@ exports.getRecord = function (callback) {
       json = {
         code: 200,
         data: [{
-          date:'2018-09-01',
-          desc:'奖励积分',
-          icon:'add',
-          pint:'20'
+          date: '2018-09-01',
+          desc: '奖励积分',
+          icon: 'add',
+          point: '20'
         }, {
           date: '2018-10-10',
           desc: '奖励积分212',
           icon: 'subtract',
-          pint: '-10'
+          point: '-10'
         }]
       }
       if (json.code == 200) {
