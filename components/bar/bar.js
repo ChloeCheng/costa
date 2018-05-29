5eb065ed6d39a4935fae7be9096f3f95a61a255d
chloe.cheng <chloe.cheng@vipshop.com>
2018-05-29 22:14:50 +0800

    检测用户信息是否授权 dialog 封装

--- components/bar/bar.js
+++ components/bar/bar.js
@@ -37,12 +37,30 @@ Component({
            }    
         },
         skipPay: function(){
-            let url = '/pages/pay/index';
+            /*let url = '/pages/pay/index';
             if(url){
               wx.navigateTo({
                  url: url
                })
-            } 
+            }*/
+            wx.addCard({
+                cardList: [
+                  {
+                    // cardId: 'piOxp1mVprjRHcshqAKU5d2jZl4U', //'pJYr5jsXWx5ckp82dPu6pVRqPfws', //'piOxp1mVprjRHcshqAKU5d2jZl4U',
+                    // cardExt:	`{"api_ticket":"HoagFKDcsGMVCIY2vOjf9ofcj2a8F5lHBS_Es91nJNj3_oDjKXET2NVfLn4UQHbMROObtZi9UXn8_-ZSFRLHcQ","code": ${code},"openid": ${id},"nonce_str":"5b0b968ad2049","signature":"d9e0a3c12811e8066631b3a21e08741d016689f7","timestamp":${time}}`
+                    
+                      cardId: 'piOxp1mVprjRHcshqAKU5d2jZl4U',
+                      cardExt: '{"code": "", "openid": "", "timestamp": "1527514580", "nonce_str":"123","signature":"d0747f9af3794a5945dd136a64c678de5a99de78"}'
+                    
+                  }
+                ],
+                success: function(res) { 
+                  console.log(res)
+                },
+                fail: function (res){
+                  console.log(res)
+                }
+              })  
          }
     }
 })
